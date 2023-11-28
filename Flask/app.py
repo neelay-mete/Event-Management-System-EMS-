60bf636fb6e173b83d7e0b12d684838b69e839d0
ashish-nagmoti <ashishnagmoti5@gmail.com>
2023-11-28 22:01:35 +0530

    login

--- Flask/app.py
+++ Flask/app.py
@@ -1,11 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('login.html')
+@app.route('/login', methods=['POST'])
+def login():
+    # Handle the login logic here
+    # You can retrieve the form data using request.form
+    username = request.form['username']
+    password = request.form['password']
+    user_type = request.form['userType']
 
+    # Add your authentication logic here (for simplicity, checking if username and password are not empty)
+    if username and password:
+        # Redirect to the appropriate route based on user type
+        if user_type == 'student':
+            return redirect(url_for('web1'))
+        elif user_type == 'teacher':
+            return redirect(url_for('web2'))
+    
+    # If authentication fails or form data is incomplete, redirect back to the login page
+    return redirect(url_for('index'))
+@app.route('/web1')
+def web1():
+    return render_template('web1.html')
 @app.route('/web2')
 def web2():
     return render_template('web2.html')
@@ -27,4 +47,4 @@ def about():
     return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, use_debugger=False, use_reloader=False)
